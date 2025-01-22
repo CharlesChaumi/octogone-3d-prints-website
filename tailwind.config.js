f51da6cb3eafd7a2ba04347cb15154aefa0ed888
Charles Chaumienne <c.chaumi@gmail.com>
2025-01-22 16:21:57 -0500

    Mise a jour des packages

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** 
+ * @copyright Technologies Octogone. Tous droits réservés
+ * Ce code est la propriété exclusive de Technologies Octogone . 
+ * Toute reproduction, utilisation ou diffusion, partielle ou totale, 
+ * sans l'autorisation écrite préalable de Technologies Octogone est strictement interdite.
+*/
+
+
+/** @type {import('tailwindcss').Config} */
+
+import tailwindScrollbar from 'tailwind-scrollbar';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontfamily: {
+        'sans': ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [tailwindScrollbar],
+}
+
